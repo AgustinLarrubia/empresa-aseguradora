f28efada3afd2c8e790d85c14ae77aaafd3bbd7b
AgustinLarrubia <agustinlarrubia@gmail.com>
2023-10-27 19:49:30 -0300

    [DONE] Se Agrego Tabla y Datos de la misma

---
+++ src/app/data/insuranceData.ts
@@ -0,0 +1,60 @@
+/* eslint-disable */
+export const DATA: any[] = [{
+  ProductID: 4,
+  ProductName: "Renault",
+  carModel: "Twingo",
+  SupplierID: 2,
+  CategoryID: 2,
+  QuantityPerUnit: "PIQ 003",
+  UnitPrice: 5000,
+  cobertura: "Responsabilidad civil",
+  UnitsInStock: 53,
+  UnitsOnOrder: 30,
+  ReorderLevel: 0,
+  Discontinued: false,
+  OrderDate: new Date("2002-03-17")
+}, {
+  ProductID: 2,
+  ProductName: "Mercedez-Benz",
+  carModel: "Sprinter",
+  SupplierID: 1,
+  CategoryID: 1,
+  QuantityPerUnit: "BB 000 YY",
+  UnitPrice: 200000,
+  cobertura: "Todo Riesgo",
+  UnitsInStock: 17,
+  UnitsOnOrder: 40,
+  ReorderLevel: 25,
+  Discontinued: true,
+  OrderDate: new Date("2017-03-17")
+}, {
+  ProductID: 3,
+  ProductName: "KTM",
+  carModel: "Duke 200",
+  SupplierID: 1,
+  CategoryID: 2,
+  QuantityPerUnit: "A 123 BCD",
+  UnitPrice: 80000,
+  cobertura: "Terceros",
+  UnitsInStock: 13,
+  UnitsOnOrder: 70,
+  ReorderLevel: 25,
+  Discontinued: true,
+  OrderDate: new Date("2020-03-17")
+}, {
+  ProductID: 1,
+  ProductName: "Ford",
+  carModel: "Ka",
+  SupplierID: 1,
+  CategoryID: 1,
+  QuantityPerUnit: "AA 000 ZZ",
+  UnitPrice: 45000,
+  cobertura: "Terceros",
+  UnitsInStock: 39,
+  UnitsOnOrder: 30,
+  ReorderLevel: 10,
+  Discontinued: true,
+  OrderDate: new Date("2016-01-02")
+}
+];
+/* tslint:enable */
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit, ViewChild } from '@angular/core';
+import { IgxGridComponent } from 'igniteui-angular';
+import { DATA } from '../data/insuranceData';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent {
 
+
+export class TableComponent implements OnInit {
+    @ViewChild('grid1', { read: IgxGridComponent, static: true })
+    public grid1: IgxGridComponent | any;
+
+    public data! : any[];
+
+    public size = 'large';
+    public sizes : any;
+
+    constructor() {
+    }
+    public ngOnInit(): void {
+        this.data = DATA;
+        this.sizes = [
+            {
+                label: 'small',
+                selected: this.size === 'small',
+                togglable: true
+            },
+            {
+                label: 'medium',
+                selected: this.size === 'medium',
+                togglable: true
+            },
+            {
+                label: 'large',
+                selected: this.size === 'large',
+                togglable: true
+            }
+        ];
+    }
+
+    @HostBinding('style.--ig-size')
+    protected get sizeStyle() {
+        return `var(--ig-size-${this.size})`;
+    }
+
+    public selectSize(event : any) {
+        this.size = this.sizes[event.index].label;
+        this.grid1.reflow();
+    }
+
+    public formatDate(val : any) {
+        if (val !== 'Select All') {
+            return new Intl.DateTimeFormat('en-US').format(val);
+        } else {
+            return val;
+        }
+    }
+
+    public formatCurrency(val: string) {
+        return parseInt(val, 10).toFixed(2);
+    }
 }
